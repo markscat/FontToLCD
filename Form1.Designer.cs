82939e634e245433f7501347de8176e401f50d51
markscat <markscat@gmail.com>
2025-08-31 15:40:13 +0800

    25.08.31
    1. 讓幾乎所有的字型都可以顯示出完整的字型
    2. 釋出為plubic

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,9 +30,7 @@
         {
             FontType = new ComboBox();
             Imput = new TextBox();
-            LedFont = new ComboBox();
             label1 = new Label();
-            label2 = new Label();
             label3 = new Label();
             BitView = new PictureBox();
             label4 = new Label();
@@ -41,8 +39,10 @@
             panel1 = new Panel();
             label5 = new Label();
             FontSize = new ComboBox();
+            scrollPanelForBitView = new Panel();
             ((System.ComponentModel.ISupportInitialize)BitView).BeginInit();
             panel1.SuspendLayout();
+            scrollPanelForBitView.SuspendLayout();
             SuspendLayout();
             // 
             // FontType
@@ -60,14 +60,6 @@
             Imput.Size = new Size(457, 27);
             Imput.TabIndex = 1;
             // 
-            // LedFont
-            // 
-            LedFont.FormattingEnabled = true;
-            LedFont.Location = new Point(700, 47);
-            LedFont.Name = "LedFont";
-            LedFont.Size = new Size(151, 27);
-            LedFont.TabIndex = 2;
-            // 
             // label1
             // 
             label1.AutoSize = true;
@@ -76,15 +68,6 @@
             label1.Size = new Size(69, 19);
             label1.TabIndex = 3;
             label1.Text = "選擇字型";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(709, 25);
-            label2.Name = "label2";
-            label2.Size = new Size(129, 19);
-            label2.TabIndex = 4;
-            label2.Text = "選擇輸出字型大小";
             // 
             // label3
             // 
@@ -97,16 +80,17 @@
             // 
             // BitView
             // 
-            BitView.Location = new Point(12, 163);
+            BitView.Location = new Point(0, 0);
             BitView.Name = "BitView";
-            BitView.Size = new Size(494, 483);
+            BitView.Size = new Size(665, 552);
+            BitView.SizeMode = PictureBoxSizeMode.AutoSize;
             BitView.TabIndex = 6;
             BitView.TabStop = false;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(152, 153);
+            label4.Location = new Point(937, 124);
             label4.Name = "label4";
             label4.Size = new Size(39, 19);
             label4.TabIndex = 7;
@@ -114,17 +98,18 @@
             // 
             // Hex
             // 
-            Hex.Location = new Point(546, 174);
+            Hex.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            Hex.Location = new Point(700, 163);
             Hex.Multiline = true;
             Hex.Name = "Hex";
             Hex.ReadOnly = true;
             Hex.ScrollBars = ScrollBars.Both;
-            Hex.Size = new Size(457, 472);
+            Hex.Size = new Size(446, 552);
             Hex.TabIndex = 8;
             // 
             // button1
             // 
-            button1.Location = new Point(909, 47);
+            button1.Location = new Point(543, 116);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 9;
@@ -133,20 +118,21 @@
             // 
             // panel1
             // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label4);
             panel1.Controls.Add(label5);
             panel1.Controls.Add(FontSize);
-            panel1.Controls.Add(Hex);
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(BitView);
-            panel1.Controls.Add(label3);
             panel1.Controls.Add(FontType);
-            panel1.Controls.Add(label2);
             panel1.Controls.Add(Imput);
-            panel1.Controls.Add(label1);
-            panel1.Controls.Add(LedFont);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(Hex);
+            panel1.Controls.Add(scrollPanelForBitView);
             panel1.Location = new Point(12, 12);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1024, 679);
+            panel1.Size = new Size(1170, 750);
             panel1.TabIndex = 10;
             // 
             // label5
@@ -166,29 +152,38 @@
             FontSize.Size = new Size(151, 27);
             FontSize.TabIndex = 10;
             // 
+            // scrollPanelForBitView
+            // 
+            scrollPanelForBitView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            scrollPanelForBitView.AutoScroll = true;
+            scrollPanelForBitView.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            scrollPanelForBitView.Controls.Add(BitView);
+            scrollPanelForBitView.Location = new Point(0, 163);
+            scrollPanelForBitView.Name = "scrollPanelForBitView";
+            scrollPanelForBitView.Size = new Size(692, 552);
+            scrollPanelForBitView.TabIndex = 12;
+            // 
             // FontToLCD
             // 
             AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1039, 703);
+            ClientSize = new Size(1187, 774);
             Controls.Add(panel1);
-            Controls.Add(label4);
             Name = "FontToLCD";
             Text = "FontToLCD";
             ((System.ComponentModel.ISupportInitialize)BitView).EndInit();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            scrollPanelForBitView.ResumeLayout(false);
+            scrollPanelForBitView.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private ComboBox FontType;
         private TextBox Imput;
-        private ComboBox LedFont;
         private Label label1;
-        private Label label2;
         private Label label3;
         private PictureBox BitView;
         private Label label4;
@@ -197,5 +192,6 @@
         private Panel panel1;
         private Label label5;
         private ComboBox FontSize;
+        private Panel scrollPanelForBitView;
     }
 }
